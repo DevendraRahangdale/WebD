ab881ce6ab0fa0197b32174de4e7865b79a8be89
Devendra Rahangdale <142200946+DevendraRahangdale@users.noreply.github.com>
2025-03-18 19:04:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const express=require("express");
-const app=express();
-const port=8080;
+/**
+ * Detect Electron renderer / nwjs process, which is node, but we should
+ * treat as a browser.
+ */
 
-app.set("view engine","ejs");
-
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
-
-
-});
-app.get("/ig/:username",(req,res)=>{
-    let {username}=req.params;
- const instadata=require("./data.json");
- const data=instadata[username];
-
- console.log(data);
-  
-
-    if(data){
-          res.render("instagram.ejs",{data});
-    }
-    else{
-        res.render("error.ejs");
-    }
-
-});
-
-
-app.get("/home",(req,res)=>{
-    res.send("Hello");
-});
-app.get("/rolldice",(req,res)=>{
-    let dicevalue=Math.floor(Math.random()*6)+1;
-    res.render("rolldice.ejs",{dicevalue});
-});
-
-
-app.listen(port,()=>{
-    console.log(`Listing on the port' ${port}`)
-})
+if (typeof process === 'undefined' || process.type === 'renderer' || process.browser === true || process.__nwjs) {
+	module.exports = require('./browser.js');
+} else {
+	module.exports = require('./node.js');
+}
